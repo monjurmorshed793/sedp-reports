772582cabb8b85890d459994f47bf8f60b321a8b
Monjur-E-Morshed <monjurmorshed793@gmail.com>
2025-09-04 23:13:48 +0600

    jupyter notebook file added

--- src/sedp_reports/grant-report.py
+++ src/sedp_reports/grant-report.py
@@ -14,5 +14,7 @@ file_path = 'data/general_information_bkp.xlsx'
 print(os.path.exists(file_path))
 
 #%%
-
 general_information = pd.read_excel('data/general_information_bkp.xlsx', engine='openpyxl')
+
+#%%
+filtered_data = general_information[general_information.eiin=='123456']
